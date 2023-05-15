ca0fcd1bad3127ca68a3ecd05672a8a26b12abc5
macie.k <maciej.kazmierczykk@gmail.com>
2023-05-15 21:39:16 +0200

    Fixes

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import cv2
 import time
 from collections import deque
 import numpy as np
+import urllib.request
 # import mediapipe as mp
 
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)
+video_url = f"http://{ip_address}:{port}/video"
+stream = urllib.request.urlopen(video_url)
+
+byte_buffer = bytes()
 
 save_last_mins = 5
 save_next_secs = 10
@@ -46,20 +50,36 @@ def gen_frames():
     global last_minutes, next_seconds
 
     while True:
-        success, frame = camera.read()
-        if not success:
-            break
+        # Read a chunk of data from the video stream
+        chunk = stream.read(1024)
 
-        detect(frame)
+        # Append the chunk to the byte buffer
+        byte_buffer += chunk
 
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
+        # Find the start and end markers of a frame
+        start_marker = byte_buffer.find(b'\xff\xd8')
+        end_marker = byte_buffer.find(b'\xff\xd9')
 
-        # add frame to last_minutes queue
-        last_minutes.append((time.time(), frame))
+        # If both markers are found, decode and display the frame
+        if start_marker != -1 and end_marker != -1:
+            frame_data = byte_buffer[start_marker:end_marker + 2]
+            byte_buffer = byte_buffer[end_marker + 2:]
 
-        # add frame in next ten seconds queue
-        next_seconds.append((time.time(), frame))
+            # Convert the frame data to a NumPy array
+            frame = np.frombuffer(frame_data, dtype=np.uint8)
+
+            # Decode the frame as an image
+            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+            detect(frame)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            # add frame to last_minutes queue
+            last_minutes.append((time.time(), frame))
+
+            # add frame in next ten seconds queue
+            next_seconds.append((time.time(), frame))
 
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
